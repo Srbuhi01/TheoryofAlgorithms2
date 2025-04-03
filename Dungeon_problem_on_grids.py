f054628b5de53857f555c29f549496f912a4a7db
Srbuhi Pupuyan <srbuhi.004@gmail.com>
2025-04-04 00:31:27 +0400

    Grid problem

---
+++ Dungeon_problem_on_grids.py
@@ -0,0 +1,40 @@
+def dungeon(grid):
+  rows, cols = len(grid), len(grid[0])
+  directions = [(0,1), (0, -1), (1, 0), (-1, 0)]
+
+  start, end = None, None
+  for r in range(rows):
+    for c in range(cols):
+      if grid[r][c] == 'S':
+        start = (r, c)
+      if grid[r][c] == 'E':
+        end = (r, c)
+  if not start or not end:
+    return "Something wrong"
+
+  queue = [(start[0], start[1], 0)]
+  visited = set([start])
+
+  while queue:
+    x, y, steps = queue.pop()
+
+    if (x, y) == end:
+      return f"Escape is possible: time` {steps} minutes"
+
+    for dx, dy in directions:
+      nx, ny = x + dx, y + dy
+      if 0 <= nx < rows and 0 <= ny <  cols and grid[nx][ny] in ('.', 'E') and (nx, ny) not in visited:
+        queue.append((nx, ny, steps + 1))
+        visited.add((nx, ny))
+  return "Escape is impossible"
+
+
+dungeon_example = [
+    ['S', '.', '.', '#', '.', '.', '.'],
+    ['.', '#', '.', '.', '.', '#', '.'],
+    ['.', '#', '.', '.', '.', '.', '.'],
+    ['.', '.', '#', '#', '.', '.', '.'],
+    ['#', '.', '#', 'E', '.', '#', '.']
+]
+
+print(dungeon(dungeon_example))
